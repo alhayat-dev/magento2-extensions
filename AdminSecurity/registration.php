9fa0dc1762fdf959f9e32d4edf8251f0c649a968
Mudasser Hayat <mudasser@royalbathrooms.co.uk>
2021-03-24 13:26:01 +0500

    Send email to owner when someone get log in from admin backend.

---
+++ AdminSecurity/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Alhayat_AdminSecurity', __DIR__);
