24fc165d974c53550291dae1c75e438de5dff9be
Mudasser Hayat <mudasser@royalbathrooms.co.uk>
2021-03-24 13:23:26 +0500

    Displayed the terms and conditions on the checkout page by overriding the knockout template.

---
+++ PaymentTerms/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    // These codes helps us to override the knockout HTML template file.
+    map: {
+        '*': {
+            'Magento_CheckoutAgreements/template/checkout/checkout-agreements.html':
+                'Alhayat_PaymentTerms/template/checkout/checkout-agreements.html'
+        }
+    }
+};
